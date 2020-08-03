399a365ce27bed24729119975bfe8df565892c47
Louis Mallez <louis.mallez@epitech.eu>
2020-08-03 16:51:18 +0100

    rework thread dispatcher
    added a queue to relieve the main thread that used to run most entities

--- wikidata-tree-generator/tree_builder/tree/dispatcher/ThreadedDispatcher.py
+++ wikidata-tree-generator/tree_builder/tree/dispatcher/ThreadedDispatcher.py
@@ -4,10 +4,27 @@ from wikidata.entity import EntityId
 from tree_builder.tree.dispatcher.Dispatcher import Dispatcher
 
 
+class ThreadExecute:
+    def __init__(self, entity_queue):
+        self.entity_queue = entity_queue
+
+    def next_entity(self):
+        return self.entity_queue.pop(0)
+
+    def execute_thread(self, method, entity_id, prof):
+        method(entity_id, prof)
+        if self.entity_queue:
+            self.execute_thread(method, self.next_entity(), prof)
+
+
 class ThreadedDispatcher(Dispatcher):
     def __init__(self, max_thread):
         self.max_thread = max_thread
         self.thread_number = 0
+
+    def start_thread(self, executer, method, entity_id, prof):
+        executer.execute_thread(method, entity_id, prof)
+        self.thread_number -= 1
 
     def compute(self, entity_ids: [EntityId], method, prof):
         if len(entity_ids) == 0:
@@ -16,17 +33,13 @@ class ThreadedDispatcher(Dispatcher):
             method(entity_ids[0], prof)
             return
         threads = []
-        other_entities = []
-        for entity_id in entity_ids:
-            if self.thread_number < self.max_thread:
-                threads.append(Thread(target=method, args=[entity_id, prof]))
-                self.thread_number += 1
-            else:
-                other_entities.append(entity_id)
+        self_id = entity_ids[0]
+        executer = ThreadExecute(entity_ids[1:])
+        while self.thread_number < self.max_thread - 1 and executer.entity_queue:
+            threads.append(Thread(target=self.start_thread, args=[executer, method, executer.next_entity(), prof]))
+            self.thread_number += 1
         for thread in threads:
             thread.start()
+        executer.execute_thread(method, self_id, prof)
         for thread in threads:
             thread.join()
-        for entity_id in other_entities:
-            method(entity_id, prof)
-        self.thread_number -= len(threads)
